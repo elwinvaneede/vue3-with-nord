5bebe53cb5d4db382d74a2255a54905a08bf5fa0
Elwin van Eede <elwinvaneede@gmail.com>
2022-07-14 08:18:03 +0200

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import "@nordhealth/components/lib/Button";
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <nord-button variant="primary">Hello</nord-button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    gap: var(--n-space-s);
+  }
+}
+</style>
